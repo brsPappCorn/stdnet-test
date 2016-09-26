d7613c0505c45419f64fb8a9429be83b4bbaa98e
David Florez <dflorez6@gmail.com>
2016-09-26 09:33:01 -0500

    V 1.5.2 -Student Profile. 	-Work experience tab in Students/show view done. 	-Work experience tab in Users/edit view done. 	-Migration to add more fields to the Student model
    ** Bug fixed on students/show. The problem, lack of validation of nil? attribute.

---
+++ db/migrate/20160926134741_add_work_experience_info_to_students.rb
@@ -0,0 +1,11 @@
+class AddWorkExperienceInfoToStudents < ActiveRecord::Migration
+  def change
+    add_column :students, :work_xp, :boolean
+    add_column :students, :xp_company, :string
+    add_column :students, :xp_position, :text
+    add_column :students, :xp_achievements, :text
+    add_column :students, :volunteer_xp, :boolean
+    add_column :students, :volunteer_org, :string
+    add_column :students, :volunteer_functions, :text
+  end
+end
